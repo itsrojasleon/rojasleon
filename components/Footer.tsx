9151e88cfc3e482fe559ccd95599323512b36a94
rojasleon <rojasleon.dev@gmail.com>
2020-10-05 13:18:59 -0500

    /

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,8 +8,10 @@ const Footer = () => {
       iconName: 'fab fa-linkedin-in',
       href: 'https://www.linkedin.com/in/rojasleon'
     },
-    { iconName: 'fab fa-youtube', href: 'https://www.youtube.com/channel/UCMmSIbY7b5S1ayAiqD8jt_g/' },
-    
+    {
+      iconName: 'fab fa-youtube',
+      href: 'https://www.youtube.com/channel/UCMmSIbY7b5S1ayAiqD8jt_g'
+    }
   ].map(({ href, iconName }) => (
     <a key={iconName} href={href} target="_blank" className="text-xl">
       <i
