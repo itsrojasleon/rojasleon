283e62905bf9c9227e2f4bf68cf60e2bfd7ce3c0
rojasleon <rojasleon.dev@gmail.com>
2022-01-19 13:12:07 -0600

    Looking for a new job

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -3,8 +3,8 @@ const Banner: React.FC = () => {
     <div className="bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">
       <div className="px-2 py-2 w-11/12 xl:w-10/12 lg-w/12 md:w-10/12 m-auto flex lg:justify-between justify-center lg:flex-row flex-col gap-3 items-center text-lg">
         <p>
-          Hey there! I'm looking for my very first serious job opportunity. Can
-          you help me?
+          Hey there! I'm looking for my next adventure, if you have an open
+          position please let me know, I may be able to help.
         </p>
         <a
           href="mailto:rojasleon.dev@gmail.com"
