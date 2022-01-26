1fcf2d348f989ed703cd83b29d1bd1ce6ca7104d
rojasleon <rojasleon.dev@gmail.com>
2022-01-25 19:21:24 -0600

    I have just found a job, let's go boys

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -6,7 +6,6 @@ const Layout: React.FC = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen bg-white dark:bg-gray-900 text-black dark:text-white">
       <Header />
-      <Banner />
       <main className="flex-grow mt-8">
         <div className="w-11/12 xl:w-10/12 lg-w/12 md:w-10/12 m-auto flex justify-center flex-col">
           {children}
